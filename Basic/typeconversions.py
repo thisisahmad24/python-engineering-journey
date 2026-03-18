7ed12127ca382e321de9998e270db86544b8fc81
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-18 21:11:51 +0500

    User Input and type conversions basics

---
+++ Basic/typeconversions.py
@@ -0,0 +1,10 @@
+age = input('Please enter your age: ')
+
+# Convert the input to an integer
+age = int(age)
+print ('Your age is:', age)
+# Convert the age back to a string
+age_str = str(age)
+print ('Your age as a string is:', age_str)
+
+print('Your age in 5 years will be:', age + 5)
