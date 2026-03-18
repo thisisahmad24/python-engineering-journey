ed56edb0c51afcf7510b0fbaccd3577befea7f66
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-18 21:08:45 +0500

    Boolean concepts basics

---
+++ Basic/boolean.py
@@ -0,0 +1,13 @@
+is_student = True
+is_logged_in = False
+
+print(is_student)
+print(is_logged_in)
+
+#Comparison
+print(5 > 3)
+print(5 < 3)
+print(5 == 5)
+print(5 != 3)
+print(5 >= 5)
+print(5 <= 3)
