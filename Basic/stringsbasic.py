b8d2816f16cfb5a89d32e0d70c0386ce652ae589
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-18 20:49:00 +0500

    it covers basic string operations

---
+++ Basic/stringsbasic.py
@@ -0,0 +1,17 @@
+name = "Ahmad"
+message = "Welcome to Python Programming!"
+
+print (name)
+print (message)
+
+#String operations
+print (name.upper())
+print (name.lower())
+print (name[0])
+print (name[1:4])
+print (len(name))
+print (len(message))
+
+#Combining strings
+greeting = "Hello, " + name + "!"
+print (greeting)
