eb5c69ab0331ecccf427448fe1e7be45ef74b573
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-18 21:18:38 +0500

    A Mini Task related User input and print on screen is done!

---
+++ Basic/minitask01.py
@@ -0,0 +1,11 @@
+#Write a program in which Ask user:
+#Name
+#Favorite programming language
+#Print:
+#Hello Ahmad!
+#Python is a powerful language to learn.
+
+name = input('Please Enter your name:')
+language = input ('Please Enter your favorite programming language:')
+print('Hello', name + '!')
+print (language, 'is a powerful language to learn.')
