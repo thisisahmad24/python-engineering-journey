200fccf1fecff6007dfb2058d800e5d868d48ffc
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-25 02:27:30 +0500

    Added Triangle Patterns

--- Basic/Patterns.py
+++ Basic/Patterns.py
@@ -15,3 +15,32 @@ for i in range (3):
         print('*' , end=" ")
     print()
     end= " "
+    
+# Right Triangle Pattern
+print('Right Triangle Pattern:')
+for i in range(6):
+    for j in range(i+1):
+        print('*', end=" ")
+    print()
+    end=" "
+    
+# Left Triangle Pattern
+print('Left Triangle Pattern:')
+for i in range(6):
+    for j in range(6):
+        if j < 6 - i -1:
+            print(' ', end=" ")
+        else:
+            print('*', end=" ")
+    print()
+    end=" "
+
+# Triangle Pattern
+print('Triangle Pattern:')
+for i in range(6):
+    for j in range(6 - i - 1):
+        print(' ', end=" ")
+    for k in range(2 * i + 1):
+        print('*', end=" ")
+    print()
+    end=" "
