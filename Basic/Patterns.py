7cd8025ad4e14fd7b587a73135f69c2305c89d92
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-25 02:18:42 +0500

    Added Square and Rectangle Patterns

--- Basic/Patterns.py
+++ Basic/Patterns.py
@@ -1,7 +1,17 @@
 #Logic Building via Patterns and Shapes
 
 # Square Pattern
+print('Square Pattern:')
 for i in range(3):
     for j in range(3):
         print ('*' , end=" ")
     print()
+    end= " "
+
+# Rectangle Pattern
+print('Rectangle Pattern:')
+for i in range (3):
+    for j in range(5):
+        print('*' , end=" ")
+    print()
+    end= " "
