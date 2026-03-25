3c837300a5c9a96a6af87039ea7741e79da0bb66
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-26 01:34:31 +0500

    Basic Level Calculator To Practice Functions and Conditions All Along with User Input

---
+++ Basic/MiniTask04.py
@@ -0,0 +1,42 @@
+# Build: Simple Calculator
+print("Welcome to Basic Calculator!")
+
+# Functions
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+
+print("Enter Your Choice, What Function You Want To Perform!")
+print("1. For Addition")
+print("2. For Subtraction")
+print("3. For Multiplication")
+print("4. For Division")
+
+choice = int(input("Enter Your Choice Here: "))
+Number1 = int(input("Enter 1st Number: "))
+Number2 = int(input("Enter 2nd Number: "))
+
+# if-else Conditions Depending on choice
+if choice ==1:
+    print(add(Number1,Number2))
+elif choice ==2:
+    print(subtract(Number1,Number2))
+elif choice ==3:
+    print(multiply(Number1,Number2))
+elif choice ==4:
+    print(divide(Number1,Number2))
+else:
+    print("Invalid User Choice!")
+    
+
+
+
