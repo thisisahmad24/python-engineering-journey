b985e980eb3ad4c8ced64736b958bb1ca06c3355
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-26 01:04:36 +0500

    Daimond and Triangle Patterns Done!

--- Basic/Patterns.py
+++ Basic/Patterns.py
@@ -44,3 +44,19 @@ for i in range(6):
         print('*', end=" ")
     print()
     end=" "
+    
+# Diamond Pattern
+print('Diamond Pattern:')
+for i in range(6):
+    for j in range(6-i-1):
+        print(' ', end=" ")
+    for k in range(2*i+1):
+        print('*', end=" ")
+    print()
+for i in range(4, -1, -1):
+    for j in range(6-i-1):
+        print(' ', end=" ")
+    for k in range(2*i+1):
+        print('*', end=" ")
+    print()
+    
