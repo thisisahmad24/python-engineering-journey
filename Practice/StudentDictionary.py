f941b800903157b686080e83fa36797e85131809
Ahmad Hassan <thisisahmad07@gmail.com>
2026-03-28 14:50:08 +0500

    Student Dictionary via user input

---
+++ Practice/StudentDictionary.py
@@ -0,0 +1,8 @@
+# Student Dictionary 
+# Take Input `name`, `age`, `course` from user → store in dictionary → print dictionary
+student = {
+    "name": input("Enter your name: "),
+    "age": int(input("Enter your age: ")),
+    "course": input("Enter your course: ")
+}
+print(student)
