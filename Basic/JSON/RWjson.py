8d62d9e79e4ae590876bbab3bbb475b2b0242b01
Ahmad Hassan <thisisahmad07@gmail.com>
2026-04-13 23:13:59 +0500

    JSON format data handling Practice

---
+++ Basic/JSON/RWjson.py
@@ -0,0 +1,13 @@
+#Read and Write JSON
+import json
+data = {"name": "Ahmad", "age": 21, "city": "Lahore"}
+
+# Writing JSON data to a file
+with open('data.json', 'w') as f:
+    json.dump(data, f)
+    
+# Reading JSON data from a file
+with open('data.json', 'r') as f:
+    loaded_data = json.load(f)
+    print(loaded_data)
+    
