944b4f86a40e0c7d590bb0bed53cadadcf45f1b6
Ahmad Hassan <thisisahmad07@gmail.com>
2026-04-17 23:47:13 +0500

    Simple Execution of Person class and objects in OOP

---
+++ OOP/simpleObj.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+        
+p1 = Person("Ali", 20)
+p2 = Person("Sara", 22)
+print(p1.name)
+print(p1.age)
+print(p2.name)
+print(p2.age)
